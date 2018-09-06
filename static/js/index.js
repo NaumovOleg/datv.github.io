af269729af4cf090defb736bf50fc27b8f800b43
Oleh <keeperoleg26@gmail.com>
2018-09-06 16:09:59 +0300

    Fix

--- static/js/index.js
+++ static/js/index.js
@@ -1,5 +1,5 @@
-//var videoUrls = [ 'videos/ident.mp4', 'videos/trans.mp4', 'videos/story (online-video-cutter.com).mp4', 'videos/second_trans.mp4', 'videos/second_story (online-video-cutter.com) (2).mp4', 'videos/outro.mp4' ];
-var videoUrls = [ './static/videos/1-Intro-Outro.webm', './static/videos/trans.mp4', './static/videos/story (online-video-cutter.com).mp4', './static/videos/second_trans.mp4', './static/videos/second_story (online-video-cutter.com) (2).mp4', './static/videos/outro.mp4' ];
+
+
 var settings = {
     videos: [],
 };
@@ -16,54 +16,51 @@ settings.videos.next = function () {
     return this[ ++this.currenIndex ];
 
 };
+
 settings.videos.currenIndex = 0;
+
+settings.videos.getNextIndex = function () {
+
+    var length = this.length - 1;
+    var currentIndex = this.currenIndex;
+
+    if ( currentIndex === length ) {
+
+        return 0;
+
+    }
+
+    return currentIndex + 1;
+
+};
+
+
 $ ( document ).ready ( function () {
 
-    window.videoContainer = $ ( '#videos-container' );
+    var videoContainers = $ ( '#videos-container' ).children ();
 
-    let videosCount = videoUrls.length;
+    for ( var i = 0; i < videoContainers.length; i++ ) {
+        var videoItem = videoContainers[ i ].childNodes[ 1 ];
+        settings.videos.push ( videoItem );
 
-    for ( var i = 0; i < videosCount; i++ ) {
+        var video = settings.videos[ i ];
 
-        var video = $ ( '<video class="' + i + '"  width="100%" height="100%" muted>' +
-            '<source src="' + videoUrls[ i ] + '" type="video/mp4"></source>' +
-            '</video>' );
-
-        var videoParent = $ ( '<div class="video-div' + (i > 0?' is-hidden':'') + '"></div> ' ).append ( video );
-        if ( i === 1 ) {
-            videoParent.append ( $ ( '<div id="wrapper"><span id="word_one"><p>WHAT' + '\'' + 'S</p></span><span id="word_two"><p>UP</p></span></div>' ) );
-        }
-
-        if ( i === 3 ) {
-            videoParent.append ( $ ( '<div id="wrapper-3"><span id="word_one"><p>Quote<br> of </p></span><span id="word_two"><p>UP</p></span></div>' ) );
-        }
-
-        settings.videos.push ( video[ 0 ] );
-
-        videoContainer.append ( videoParent[ 0 ] );
-
-        video.on ( 'ended', function () {
+        video.addEventListener ( 'ended', function () {
 
             settings.videos[ settings.videos.currenIndex ].parentElement.classList.add ( 'is-hidden' );
-            var nextVideo = settings.videos.next ();
-            nextVideo.currentTime = 0.15;
-            nextVideo.play ();
-            nextVideo.parentElement.classList.remove ( 'is-hidden' );
+            settings.videos.next ();
+
         } );
-    };
+
+    }
 
 
-    console.log( settings );
-
-    runAll();
+    runAll ();
 
 } );
 
 
 function runAll () {
-
-    console.log( settings )
-
     settings.videos.forEach ( function ( video ) {
 
         video.play ();
@@ -75,55 +72,50 @@ function runAll () {
 
             if ( index !== 0 ) {
                 video.pause ();
-                video.currentTime = 0.15;
+                video.currentTime = 0;
             }
 
-            if ( index === 1 ) {
-                video.currentTime = 0;
-                video.ontimeupdate = function () {
-
-                    if ( this.currentTime > 0.1 ) {
-
+            video.ontimeupdate = function () {
+                if ( this.currentTime >= (this.duration - 0.7) ) {
+                    settings.videos[ settings.videos.getNextIndex () ].parentElement.classList.remove ( 'is-hidden' );
+                    settings.videos[ settings.videos.getNextIndex () ].play ();
+                }
+                ;
+                if ( index === 1 ) {
+                    if ( this.currentTime > 0.2 ) {
                         $ ( '#wrapper' ).css ( { display: 'block' } );
-
-                        if ( this.currentTime >= (this.duration - 0.5) ) {
+                        if ( this.currentTime >= ( this.duration - 0.5) ) {
                             $ ( '#wrapper' ).css ( { display: 'none' } );
                         }
-
                     }
-                };
+                }
+                ;
 
-            }
-
-            if ( index === 3 ) {
-                video.currentTime = 0;
-                video.ontimeupdate = function () {
-
-                    if ( this.currentTime > 3 ) {
-
-                        $ ( '#wrapper-3' ).css ( { display: 'block' } );
-
+                if ( index === 2 ) {
+                    var line = $ ( '.line-animation__with-timer' )[ 0 ];
+                    if ( this.currentTime > 13 ) {
+                        line.style.display = 'block';
                         if ( this.currentTime >= (this.duration - 0.5) ) {
-                            $ ( '#wrapper-3' ).css ( { display: 'none' } );
+                            line.style.display = 'none';
                         }
-
                     }
-                };
+                }
 
-            }
+                if ( index === 3 ) {
+                    if ( this.currentTime > 2.5 ) {
+                        $ ( '.animation__3' )[ 0 ].style.display = 'block';
 
-
+                        if ( this.currentTime >= ( this.duration - 0.5) ) {
+                            $ ( '.animation__3' )[ 0 ].style.display = 'none';
+                        }
+                    }
+                }
+                ;
+            };
         } );
     }, 500 );
-
-
-    console.log( settings );
 
     settings.videos[ 0 ].currentTime = 0.15;
     settings.videos[ 0 ].play ();
 
-
-
 }
-
-
